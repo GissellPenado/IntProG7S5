5eccff5eaf442f4cfbdddac118d6849937f8b6ff
GissellPenado <gpenadom@uamv.edu.ni>
2025-04-21 11:40:53 -0600

    Ejercicio sobre rangos

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+rango_inicial=int(input("Escribe el inicio del rango: "))
+rango_final=int(input("Escribe el final del rango: "))
+
+if(rango_inicial<rango_final):
+    numero=int(input("Dime un numero: "))
+    if(numero >=rango_inicial and numero<=rango_final):
+        print("El número está dentro del rango.")
+    else:
+        print("El número está fuera del rango.")
+else:
+    print("El rango inicial debe ser menor que el rango final")
