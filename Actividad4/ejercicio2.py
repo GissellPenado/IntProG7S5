c93167191ed9bdba1bb3f5b5495ed5cf752d77f9
GissellPenado <gpenadom@uamv.edu.ni>
2025-04-27 11:30:08 -0600

    Modifiqué ejercicio2

--- Actividad4/ejercicio2.py
+++ Actividad4/ejercicio2.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+import datetime
 fecha_ultimo_inicio=input("Ingrese la fecha de ultimo inicio (AAAA-MM-DD): ")
-fecha_ultimo_inicio=datetime.strptime(fecha_ultimo_inicio, "%Y-%m-%d")
-fecha_actual=datetime.now()
+fecha_ultimo_inicio=datetime.datetime.strptime(fecha_ultimo_inicio, "%Y-%m-%d")
+fecha_actual=datetime.datetime.now()
 dias_transcurridos=(fecha_actual-fecha_ultimo_inicio).days
 if dias_transcurridos>30:
     print("Cuenta inactiva")
